894803089c3d5588742704b342daa88a337fb81b
warador <svaskevich@gmail.com>
2022-12-01 23:50:19 +0300

    DZ4 complete

---
+++ DZ4/Program.cs
@@ -0,0 +1,68 @@
+﻿Console.WriteLine("Поиск максимального числа.");
+
+Console.WriteLine("Введите первое число!");
+int numberA = Convert.ToInt32( Console.ReadLine());
+
+Console.WriteLine("Введите второе число!");
+int numberB = Convert.ToInt32( Console.ReadLine());
+
+Console.WriteLine("Введите третье число!");
+int numberC = Convert.ToInt32( Console.ReadLine());
+
+int max = 0;
+
+if(numberA > numberB)
+{
+    max = numberA;
+}
+else 
+{
+    max = numberB;
+}
+if(numberC > max)
+{
+    max = numberC;
+}
+else
+{
+   Console.WriteLine("Они равны!");   
+}
+Console.WriteLine("Максимальное число равно ");
+Console.Write(max);
+
+
+
+
+//Console.WriteLine("Поиск максимального числа.");
+
+//Console.WriteLine("Введите первое число!");
+//int numberA = Convert.ToInt32( Console.ReadLine());
+
+//Console.WriteLine("Введите второе число!");
+//int numberB = Convert.ToInt32( Console.ReadLine());
+
+//Console.WriteLine("Введите третье число!");
+//int numberC = Convert.ToInt32( Console.ReadLine());
+
+//if(numberA > numberB  &  numberA > numberC)
+//{
+//    Console.Write("Максимальное число ");
+//    Console.WriteLine( numberA);
+//}
+
+//else if(numberB > numberA  &  numberB > numberC)
+//{
+//    Console.Write("Максимальное число ");
+//    Console.WriteLine( numberB);
+//}
+
+//else if(numberB < numberC  &  numberA < numberC)
+//{
+//    Console.Write("Максимальное число ");
+//    Console.WriteLine( numberC);
+//}
+
+//else
+//{
+//    Console.Write("Они равны!");   
+//}
