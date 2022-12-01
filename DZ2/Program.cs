64eb69ea0706c46bef217f1bf7de518ff2d1b5b4
warador <svaskevich@gmail.com>
2022-12-01 22:55:08 +0300

    DZ2 complete

--- DZ2/Program.cs
+++ DZ2/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Поиск максимального числа.");
+
+Console.WriteLine("Введите первое число!");
+int numberA = Convert.ToInt32( Console.ReadLine());
+
+Console.WriteLine("Введите второе число!");
+int numberB = Convert.ToInt32( Console.ReadLine());
+
+if(numberA < numberB)
+{
+    Console.Write("Максимальное число ");
+    Console.WriteLine( numberB);
+}
+
+else if(numberA > numberB)
+{
+    Console.Write("Максимальное число ");
+    Console.WriteLine( numberA);
+}
+
+else
+{
+    Console.Write("Они равны!");   
+}
+
+
