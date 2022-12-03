a6a7fae7f43d682c3967fcdf44b1e19c6e3ad0a0
warador <svaskevich@gmail.com>
2022-12-03 17:48:46 +0300

    DZ8 вывод всех четных чисел до N

---
+++ DZ8/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Поиск всех четных чисел.");
+int a = 1;
+Console.WriteLine("Введите число!");
+int n = Convert.ToInt32( Console.ReadLine());
+
+while (a < n)
+    if(a % 2 == 0)
+    {
+        Console.WriteLine(a);
+        a++;
+    }
+    else 
+    {
+        a++;
+    }
+  
