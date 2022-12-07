8ceb914eee88037f803553039dff047073827957
warador <svaskevich@gmail.com>
2022-12-07 20:16:33 +0300

    DZ8 вывод второго числа из трехзначного

---
+++ DZ1/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+
+Console.WriteLine("Поиск второй цифры из трехзначного числа.");
+
+Console.WriteLine("Введите число!");
+int numberA = Convert.ToInt32( Console.ReadLine());
+
+
+if(numberA < 999  &  numberA > 99)
+{
+   int first = numberA / 10;
+   int second = first % 10;
+Console.Write(second);
+}
+
+else 
+{
+   Console.Write("Число не трехзначное! ");
+}
+
