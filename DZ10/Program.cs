72aa0ee8a2006f60bee9d8e9dfc59678b6522026
warador <svaskevich@gmail.com>
2022-12-11 01:27:28 +0300

    DZ10 Создание N массива и возведение его чисел в куб

---
+++ DZ10/Program.cs
@@ -0,0 +1,41 @@
+﻿// Напишите программу, которая принимает на вход число (N) и помещает в массив таблицу кубов чисел от 1 до N. Полученный массив вывести на экран.
+// 3 -> [1, 8, 27].
+// 5 -> [1, 8, 27, 64, 125]
+// Указание: Вывод массива вынести в отдельную функцию.
+
+
+
+
+int n = Convert.ToInt32( Console.ReadLine());
+int[] array = new int[n];
+void PrintArray()
+{
+    for (int i = 0; i < array.Length; i++)
+        {
+            Console.WriteLine(array[i]);   
+        }
+}
+
+for (int i = 0; i < n; i++)
+     {
+        array[i] = i;
+        array[i] = (i+1) * (i+1) * (i+1);
+     }
+
+
+
+PrintArray();
+
+
+
+
+
+
+
+
+
+
+
+
+// Console.WriteLine("______________________");
+// Console.WriteLine("{0}", string.Join("\n", array));
