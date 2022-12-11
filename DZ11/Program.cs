05e37dd2baf9df97122b52dadaf451e51f74a48e
warador <svaskevich@gmail.com>
2022-12-11 17:06:13 +0300

    DZ11 Возведение числа А в степень B)

---
+++ DZ11/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. Не использовать функцию Math.Pow
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+Console.WriteLine("Введите A!");
+int A = Convert.ToInt32( Console.ReadLine());
+Console.WriteLine("_______________________");
+Console.WriteLine("Введите B!");
+int B = Convert.ToInt32( Console.ReadLine());
+Console.WriteLine("_______________________");
+int temp = A;
+int result = 0;
+
+for (int i = 1; i < B; i++)
+     {
+        temp = temp * A;
+        result = temp;
+     }
+
+Console.Write("Ответ = ");
+Console.WriteLine(result);
