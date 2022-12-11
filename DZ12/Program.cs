902909681d3997cd24b8771d3649e56f1444668f
warador <svaskevich@gmail.com>
2022-12-11 19:02:02 +0300

    DZ12 Сумма делителей числа)

---
+++ DZ12/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе, которые являются делителями этого числа.
+
+// 452 -> 6
+
+// 82 -> 2
+
+// 9012 -> 3
+
+Console.WriteLine("Введите число!");
+int n = Convert.ToInt32( Console.ReadLine());
+Console.WriteLine("_____________");
+int result = 0;
+int temp = n;
+for (int i = 2; i < 10; i++)
+{
+    temp = n / i;
+    temp = temp % 10;
+    
+    if (temp == 0)
+    {
+        result = result + i;
+    
+    }
+}
+Console.WriteLine(result);
