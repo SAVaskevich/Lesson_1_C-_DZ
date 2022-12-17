0f9e9e85635d847290f639081ee125e5dbacaf9b
warador <svaskevich@gmail.com>
2022-12-17 23:17:12 +0300

    DZ13 Заполнение массива и поиск четных чисел в нем)

---
+++ DZ13/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+
+int result = 0;
+int[] array = new int[4];
+Random random = new Random();
+Console.Write("Массив: ");
+for (int i = 0; i < array.Length; i++)
+    {
+    array[i] = random.Next(100,999);               
+    Console.Write("{0} ", array[i]);               
+    }
+            
+for (int i = 0; i < array.Length; i++)
+    {                              
+    if(array[i] % 2 == 0)
+        {
+        result += 1;
+        }
+
+    }           
+Console.WriteLine();          
+Console.Write("Количество четных чисел: ");
+Console.WriteLine(result);
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            // Console.WriteLine();
+            // Console.WriteLine();
+            // Console.Write("Индексы: ");
+            // for (int i = 0; i < array.Length; i++)
+            // {
+            //     Console.Write("{0} ", i + 1);
+            // }
+            // Console.WriteLine();
