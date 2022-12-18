4499a6d09dea2979b7f7d94da27e02715b3ea61e
warador <svaskevich@gmail.com>
2022-12-18 15:22:43 +0300

    DZ13 Заполнение массива и поиск четных чисел в нем)

--- DZ13/Program.cs
+++ DZ13/Program.cs
@@ -7,19 +7,19 @@ int[] array = new int[4];
 Random random = new Random();
 Console.Write("Массив: ");
 for (int i = 0; i < array.Length; i++)
-    {
-    array[i] = random.Next(100,999);               
-    Console.Write("{0} ", array[i]);               
-    }
-            
-for (int i = 0; i < array.Length; i++)
-    {                              
-    if(array[i] % 2 == 0)
-        {
-        result += 1;
-        }
+{
+    array[i] = random.Next(100, 999);
+    Console.Write("{0} ", array[i]);
+}
 
-    }           
-Console.WriteLine();          
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] % 2 == 0)
+    {
+        result += 1;
+    }
+
+}
+Console.WriteLine();
 Console.Write("Количество четных чисел: ");
 Console.WriteLine(result);
