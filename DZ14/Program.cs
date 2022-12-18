3cdb8b704e788a0fc4ab4dc74c89710e082983fd
warador <svaskevich@gmail.com>
2022-12-18 15:21:34 +0300

    DZ14 сумма элементов массива на не четных позициях.

--- DZ14/Program.cs
+++ DZ14/Program.cs
@@ -5,18 +5,18 @@ int[] array = new int[4];
 Random random = new Random();
 Console.Write("Массив: ");
 for (int i = 0; i < array.Length; i++)
-    {
-        array[i] = random.Next(-10,99);               
-        Console.Write("{0} ", array[i]);               
-    }           
+{
+    array[i] = random.Next(-10, 99);
+    Console.Write("{0} ", array[i]);
+}
 for (int i = 0; i < array.Length; i++)
-    {                              
-        if (i % 2 != 0)
-        {
-            result = result + array[i];
-        }  
-    }          
-Console.WriteLine();          
+{
+    if (i % 2 != 0)
+    {
+        result = result + array[i];
+    }
+}
+Console.WriteLine();
 Console.Write("Сумма чисел массива на не четных индексах: ");
 Console.WriteLine(result);
 
