9a428822c4d76b02aecaa1b5260cc2c5dea7914f
warador <svaskevich@gmail.com>
2022-12-18 12:06:18 +0300

    DZ14 сумма элементов массива на не четных позициях.)

---
+++ DZ14/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+int result = 0;
+int[] array = new int[4];
+Random random = new Random();
+Console.Write("Массив: ");
+for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = random.Next(-10,99);               
+        Console.Write("{0} ", array[i]);               
+    }           
+for (int i = 0; i < array.Length; i++)
+    {                              
+        if (i % 2 != 0)
+        {
+            result = result + array[i];
+        }  
+    }          
+Console.WriteLine();          
+Console.Write("Сумма чисел массива на не четных индексах: ");
+Console.WriteLine(result);
+
