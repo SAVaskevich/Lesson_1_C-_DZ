d01946633f6fba7f3cc9bb7d86907f9bbd02bfab
warador <svaskevich@gmail.com>
2022-12-28 20:28:10 +0300

    DZ1 1.0

--- DZ1/Program.cs
+++ DZ1/Program.cs
@@ -9,18 +9,14 @@
 Console.WriteLine("Поиск второй цифры из трехзначного числа.");
 
 Console.WriteLine("Введите число!");
-int numberA = Convert.ToInt32( Console.ReadLine());
+int numberA = Convert.ToInt32(Console.ReadLine());
 
 
-if(numberA < 999  &  numberA > 99)
-{
-   int first = numberA / 10;
-   int second = first % 10;
-Console.Write(second);
-}
-
-else 
-{
+if (numberA < 999 & numberA > 99) {
+    int first = numberA / 10;
+    int second = first % 10;
+    Console.Write(second);
+} else {
    Console.Write("Число не трехзначное! ");
 }
 
